fb9366c8aca6e68da8c70fd632faf5a41715f1ad
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-27 10:15:42 -0300

    feat: add userController in router

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 
+const userController = require('../controllers/user.controller');
+
 const router = Router();
 
 router.get('/', (_req, res)=> {
     res.status(200).json('users');
 })
+
+router.post('/', userController.createUser);
 
 module.exports = router;
